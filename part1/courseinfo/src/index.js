2d98fedcb4e224bb124b93e440a881c0c92090c7
Néstor Liberato <nestitor87@gmail.com>
2023-01-26 13:54:51 +0100

    Use createRoot

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import Header from './Header'
 import Total from './Total'
 import Content from './Content'
@@ -28,12 +28,10 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-); */
+);
 
