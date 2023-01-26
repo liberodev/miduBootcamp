d01013cf468651a10ad9ef8f5ea9084dcd02846d
Néstor Liberato <nestitor87@gmail.com>
2023-01-26 13:54:12 +0100

    Add step 3

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -5,15 +5,11 @@ const Content = (props) => {
   const {part2} = props
   const {part3} = props
 
-  const {exercises1} = props
-  const {exercises2} = props
-  const {exercises3} = props
-
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   )
 }
