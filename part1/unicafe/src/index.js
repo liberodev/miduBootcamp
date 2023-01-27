6b02f2235bb1e48aa3aa30a21e5ee0db257278f5
Néstor Liberato <nestitor87@gmail.com>
2023-01-27 12:26:03 +0100

    Add 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -20,6 +20,19 @@ const Statistics = ({good, neutral, bad}) => {
     </div>
   )
 }
+const Total = ({total}) => <p>all {total}</p>
+const Average = ({avg}) => {
+  if (isNaN(avg))
+    return <p>average 0</p>
+  else
+    return <p>average {avg.toFixed(2)}</p>
+}
+const Positive = ({positive}) => {
+  if (isNaN(positive))
+    return <p>positive 0</p>
+  else
+    return <p>positive {positive.toFixed(2)} %</p>
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -33,6 +46,9 @@ const App = () => {
       <Feedback good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad} />
       <Cabecera texto='statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <Total total={good + neutral + bad} />
+      <Average avg={((good*1) + (neutral*0) + (bad*(-1))) / (good + neutral + bad)} />
+      <Positive positive={(good*100) / (good + neutral + bad)} />
     </div>
   )
 }
