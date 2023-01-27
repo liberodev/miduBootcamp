a3c2a2034d4d95abdc4ee17466f3903e9e163dfc
Néstor Liberato <nestitor87@gmail.com>
2023-01-27 12:46:01 +0100

    Add 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Cabecera = ({texto}) => <h1>{texto}</h1>
-const Feedback = ({good, setGood, neutral, setNeutral, bad, setBad}) => {
+const Button = ({good, setGood, neutral, setNeutral, bad, setBad}) => {
   return (
     <div>
       <button onClick={() => setGood(good + 1)}>good</button>
@@ -12,33 +12,25 @@ const Feedback = ({good, setGood, neutral, setNeutral, bad, setBad}) => {
   )
 }
 const Statistics = ({good, neutral, bad}) => {
-  if ( good === 0 && neutral === 0 && bad === 0)
+  const total = good + neutral + bad
+  const dividendoAVG = (good*1) + (neutral*0) + (bad*(-1))
+  const dividendoPOS = good*100
+
+  if ( total === 0)
     return <p>No feedback given</p>
 
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <Total total={good + neutral + bad} />
-      <Average avg={((good*1) + (neutral*0) + (bad*(-1))) / (good + neutral + bad)} />
-      <Positive positive={(good*100) / (good + neutral + bad)} />
+      <Statistic text='good' value={good} />
+      <Statistic text='neutral' value={neutral} />
+      <Statistic text='bad' value={bad} />
+      <Statistic text='all' value={total} />
+      <Statistic text='average' value={isNaN(dividendoAVG / total) ? 0 : dividendoAVG / total} />
+      <Statistic text='positive' value={isNaN(dividendoPOS / total) ? 0 : dividendoPOS / total} />
     </div>
   )
 }
-const Total = ({total}) => <p>all {total}</p>
-const Average = ({avg}) => {
-  if (isNaN(avg))
-    return <p>average 0</p>
-  else
-    return <p>average {avg.toFixed(2)}</p>
-}
-const Positive = ({positive}) => {
-  if (isNaN(positive))
-    return <p>positive 0</p>
-  else
-    return <p>positive {positive.toFixed(2)} %</p>
-}
+const Statistic = ({text, value}) => <p>{text} {value}</p>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -49,7 +41,7 @@ const App = () => {
   return (
     <div>
       <Cabecera texto='give feedback' />
-      <Feedback good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad} />
+      <Button good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad} />
       <Cabecera texto='statistics' />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
