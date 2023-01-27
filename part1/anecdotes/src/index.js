716ad348494a717a6154e8d99b6bf1d535fb70e3
Néstor Liberato <nestitor87@gmail.com>
2023-01-27 14:04:51 +0100

    Add 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom/client'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0
+  })
   const longitud = anecdotes.length
-  const handleClick = () => setSelected(Math.floor(Math.random()*longitud))
+  const handleClickAnecdote = () => setSelected(Math.floor(Math.random()*longitud))
+  const handleClickVote = () => {
+    const copy = {...points}
+    copy[selected]++
+
+    setPoints({...copy})
+  }
 
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
+      <p>Has {points[selected]} votes</p>
       <br />
-      <button onClick={handleClick}>Next anecdote</button>
+      <button onClick={handleClickVote}>Vote</button>
+      <button onClick={handleClickAnecdote}>Next anecdote</button>
     </div>
   )
 }
@@ -24,9 +40,10 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App anecdotes={anecdotes} />
+    <App anecdotes={anecdotes}/>
   </React.StrictMode>
 );
