846cb1d50c1bc8a491776b754f13ce539454d99a
Néstor Liberato <nestitor87@gmail.com>
2023-01-27 12:04:18 +0100

    Add ex. 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Cabecera = ({texto}) => <h1>{texto}</h1>
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Cabecera texto='give feedback' />
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Cabecera texto='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
